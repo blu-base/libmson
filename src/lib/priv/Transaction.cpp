32993d6d8476db1ee110920d708c7515795b4249
Sebastian Engel <dev@sebastianengel.eu>
2020-11-24 14:22:08 +0100

    Make Transaction.cpp passive for now

--- src/lib/priv/Transaction.cpp
+++ src/lib/priv/Transaction.cpp
@@ -1,33 +1,34 @@
-#include "Transaction.h"
-namespace libmson {
-namespace priv {
+//#include "Transaction.h"
+// namespace libmson {
+// namespace priv {
 
-Transaction::Transaction()
-{
+// Transaction::Transaction()
+//{
 
-}
-
-
-std::list<TransactionEntry_WPtr_t> Transaction::getEntries() const
-{
-  return m_entries;
-}
-
-void Transaction::setEntries(const std::list<TransactionEntry_WPtr_t>& entries)
-{
-  m_entries = entries;
-}
-
-quint32 Transaction::getCrc() const
-{
-  return m_crc;
-}
-
-void Transaction::setCrc(const quint32& crc)
-{
-  m_crc = crc;
-}
+//}
 
 
-} // namespace priv
-} // namespace libmson
+// std::list<TransactionEntry_WPtr_t> Transaction::getEntries() const
+//{
+//  return m_entries;
+//}
+
+// void Transaction::setEntries(const std::list<TransactionEntry_WPtr_t>&
+// entries)
+//{
+//  m_entries = entries;
+//}
+
+// quint32 Transaction::getCrc() const
+//{
+//  return m_crc;
+//}
+
+// void Transaction::setCrc(const quint32& crc)
+//{
+//  m_crc = crc;
+//}
+
+
+//} // namespace priv
+//} // namespace libmson
